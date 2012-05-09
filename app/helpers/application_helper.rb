3721b60b62dfae5fb013234baac4d9c1da837bef
Camel Song <neosoyn@gmail.com>
2012-05-09 19:00:00 +0800

    add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Focus Mail"
+    if page_title.empty?
+      base_title
+    else
+      %Q{#{base_title} | #{page_title}}
+    end
+  end
 end
