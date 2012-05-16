382850673f846c64638de8024635b50687377d41
Camel Song <neosoyn@gmail.com>
2012-05-16 19:18:01 +0800

    ad Campaign theme

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,6 +3,7 @@ class CampaignsController < ApplicationController
   # GET /campaigns.json
   def index
     @campaigns = Campaign.all
+    @campaign = Campaign.new
 
     respond_to do |format|
       format.html # index.html.erb
@@ -21,63 +22,54 @@ class CampaignsController < ApplicationController
     end
   end
 
-  # GET /campaigns/new
-  # GET /campaigns/new.json
   def new
     @campaign = Campaign.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @campaign }
+      format.js
     end
   end
 
-  # GET /campaigns/1/edit
   def edit
     @campaign = Campaign.find(params[:id])
   end
 
-  # POST /campaigns
-  # POST /campaigns.json
   def create
     @campaign = Campaign.new(params[:campaign])
 
     respond_to do |format|
       if @campaign.save
         format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
-        format.json { render json: @campaign, status: :created, location: @campaign }
+        format.js
       else
         format.html { render action: "new" }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+        format.js { render action: "new" }
       end
     end
   end
 
-  # PUT /campaigns/1
-  # PUT /campaigns/1.json
   def update
     @campaign = Campaign.find(params[:id])
 
     respond_to do |format|
       if @campaign.update_attributes(params[:campaign])
         format.html { redirect_to @campaign, notice: 'Campaign was successfully updated.' }
-        format.json { head :no_content }
+        format.js
       else
         format.html { render action: "edit" }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+        format.js { render action: "edit" }
       end
     end
   end
 
-  # DELETE /campaigns/1
-  # DELETE /campaigns/1.json
   def destroy
     @campaign = Campaign.find(params[:id])
     @campaign.destroy
 
     respond_to do |format|
       format.html { redirect_to campaigns_url }
-      format.json { head :no_content }
+      format.js
     end
   end
 end
