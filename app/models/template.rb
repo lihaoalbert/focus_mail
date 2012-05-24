7500466f944c99877e3e792a006590a478d73f64
Camel Song <neosoyn@gmail.com>
2012-05-24 17:18:12 +0800

    模板页面添加编辑源码功能

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,5 +1,13 @@
 class Template < ActiveRecord::Base
-  attr_accessible :file_name, :name
+  attr_accessible :file_name, :name, :source
   has_many :entries
   has_many :campaigns
+
+  def source
+    @source = IO.readlines(File.expand_path("../../views/member_mailer/#{self.file_name}.html.erb", __FILE__)).join("").strip
+  end
+
+  def source= (val)
+    File.open(File.expand_path("../../views/member_mailer/#{self.file_name}.html.erb", __FILE__), 'wb'){ |f| f.write(val) }
+  end
 end
